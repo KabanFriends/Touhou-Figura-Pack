8cd78aa14bcb127c3064a55b79f139dcc20b765e
KabanFriends <yuukun2004@protonmail.ch>
2021-10-01 18:22:34 +0900

    karakasa

---
+++ karakasa/script.lua
@@ -0,0 +1,12 @@
+--setup models
+model.all.setScale({1.3,1.3,1.3})
+
+vec = {0,18.5,0}
+for key, value in pairs(model.all) do
+	if (type(value) == "table") then
+		value.setPos(vec)
+	end
+end
+
+vec = {0,-5.5,0}
+model.all.Body.setPos(vec)
