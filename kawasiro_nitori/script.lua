69dca7b2e512276304838a820ce8b758b3bb6784
KabanFriends <yuukun2004@protonmail.ch>
2021-10-01 18:00:20 +0900

    kawasiro_nitori

---
+++ kawasiro_nitori/script.lua
@@ -0,0 +1,34 @@
+--hide default player skin
+for key, value in pairs(vanilla_model) do
+	value.setEnabled(false)
+end
+
+--setup models
+model.all.setScale({1.3,1.3,1.3})
+
+model.all.RightArm.setRot({0, 0, 0})
+model.all.LeftArm.setRot({0, 0, 0})
+
+vec = {0,18.5,0}
+for key, value in pairs(model.all) do
+	if (type(value) == "table") then
+		value.setPos(vec)
+	end
+end
+
+vec = {0,-5.5,0}
+model.all.Head.setPos(vec)
+model.all.Body.setPos(vec)
+model.all.RightArm.setPos(vec)
+model.all.LeftArm.setPos(vec)
+model.all.RightLeg.setPos(vec)
+model.all.LeftLeg.setPos(vec)
+
+--sitting animation
+function render()
+	if (player.getVehicle() ~= nil) then
+		model.all.Body.sittingRotationSkirt.setRot({25, 0, 0})
+	else
+		model.all.Body.sittingRotationSkirt.setRot({0, 0, 0})
+	end
+end
