8374991fd0eedb95876b9ccbe34a51cf05438898
KabanFriends <yuukun2004@protonmail.ch>
2021-10-01 18:13:29 +0900

    tenshi_hat

---
+++ tenshi_hat/script.lua
@@ -0,0 +1,11 @@
+--setup models
+
+vec = {0,18.5,0}
+for key, value in pairs(model.all) do
+	if (type(value) == "table") then
+		value.setPos(vec)
+	end
+end
+
+vec = {0,-5.5,0}
+model.all.Head.setPos(vec)
