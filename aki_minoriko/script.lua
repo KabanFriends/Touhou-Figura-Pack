dbad5742987d178caab4b2ee934c2ff40e237495
KabanFriends <yuukun2004@protonmail.ch>
2023-08-04 03:10:15 +0900

    Fix skull (aki_minoriko)

--- aki_minoriko/script.lua
+++ aki_minoriko/script.lua
@@ -2,19 +2,19 @@
 vanilla_model.PLAYER:setVisible(false)
 
 --setup models
-models:scale(1.3, 1.3, 1.3)
+models.player_model:scale(1.3, 1.3, 1.3)
 
 models.player_model.all.RightArm:setRot(0, 0, 0)
 models.player_model.all.LeftArm:setRot(0, 0, 0)
 
 models.player_model.all.Head.blink:setVisible(false)
 
-vec = vectors.vec3(0, 18.5, 0)
-for key, value in pairs(models.player_model.all:getChildren()) do
-	if (type(value) == "table") then
-		value:setPos(vec)
-	end
-end
+--setup skull
+local part = models.player_model.all.Head:copy("SkullModel")
+part:setParentType("None")
+part:setPos(0, -18, 0)
+part:removeChild(part.blink)
+models.skull.Skull:addChild(part)
 
 --blinking animation
 local blinkTimer = 0
@@ -37,6 +37,7 @@ end)
 
 --sitting animation
 events.RENDER:register(function(delta, ctx)
+	models.player_model.all.Head:setPos(0, 0, 0)
 	if (player:getVehicle() ~= nil) then
 		models.player_model.all.Body.sittingRotationSkirt:setRot(25, 0, 0)
 	else
