bd366a0bf242efde9bb46bacc0e5fbac8ecd535e
KabanFriends <yuukun2004@protonmail.ch>
2023-08-04 15:34:05 +0900

    model porting - round 2

---
+++ .scripts/components/setup_noblink.lua
@@ -0,0 +1,14 @@
+--hide default player skin
+vanilla_model.PLAYER:setVisible(false)
+
+--setup models
+models.player_model:scale(1.3, 1.3, 1.3)
+
+models.player_model.all.RightArm:setRot(0, 0, 0)
+models.player_model.all.LeftArm:setRot(0, 0, 0)
+
+--setup skull
+local part = models.player_model.all.Head:copy("SkullModel")
+part:setParentType("None")
+part:setPos(0, -18, 0)
+models.skull.Skull:addChild(part)
