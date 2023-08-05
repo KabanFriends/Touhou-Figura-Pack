3e1d3e5311a86f8b3e87f294532fd983cdd0173b
KabanFriends <yuukun2004@protonmail.ch>
2023-08-05 16:27:33 +0900

    shikieiki scripting

---
+++ shikieiki_yamaxanadu/script.lua
@@ -0,0 +1,16 @@
+--hide the chestPlate bones when offhand item exists
+events.RENDER:register(function (delta, context)
+	if (player:isLeftHanded() == true) then
+		if (player:getHeldItem(false).id ~= "minecraft:air") then
+			models.player_model.all.LeftArm.chestPlate:setVisible(false)
+		else
+			models.player_model.all.LeftArm.chestPlate:setVisible(true)
+		end
+	else
+		if (player:getHeldItem(true).id ~= "minecraft:air") then
+			models.player_model.all.LeftArm.chestPlate:setVisible(false)
+		else
+			models.player_model.all.LeftArm.chestPlate:setVisible(true)
+		end
+	end
+end)
