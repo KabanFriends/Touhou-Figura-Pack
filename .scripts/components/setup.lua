30e0da4db0d01730df56abca68837600000a6d1b
KabanFriends <yuukun2004@protonmail.ch>
2023-08-05 17:46:38 +0900

    modify first person hand position

--- .scripts/components/setup.lua
+++ .scripts/components/setup.lua
@@ -1,11 +1,26 @@
+--variables
+local scale = 1.3
+local unscale = 1/scale
+
 --hide default player skin
 vanilla_model.PLAYER:setVisible(false)
 
 --setup models
-models.player_model:scale(1.3, 1.3, 1.3)
+models.player_model:scale(scale)
 
 models.player_model.all.RightArm:setRot(0, 0, 7)
 models.player_model.all.LeftArm:setRot(0, 0, -7)
+
+--setup hand items
+local rightitem = models.player_model.all.RightArm:newPart("RightItemPivot")
+rightitem:setParentType("RightItemPivot")
+rightitem:setPivot(4, 8.5, -1)
+rightitem:setScale(unscale)
+
+local leftitem = models.player_model.all.LeftArm:newPart("LeftItemPivot")
+leftitem:setParentType("LeftItemPivot")
+leftitem:setPivot(-4, 8.5, -1)
+leftitem:setScale(unscale)
 
 --setup skull
 local skullmodel = models.player_model.all.Head:copy("SkullModel")
